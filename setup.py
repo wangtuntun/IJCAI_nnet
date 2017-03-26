4f245cf9ddf46b984a8c749548345a2015f57848
wangtuntun <wtt561111@163.com>
2017-03-26 17:04:50 +0800

    commit files

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(
+  name = 'nnet-ts',
+  packages = ['nnet_ts'],
+  version = '0.6',
+  description = 'Neural network architecture for time series forecasting.',
+  author = 'José Jiménez',
+  author_email = 'jose@jimenezluna.com',
+  url = 'https://github.com/hawk31/nnet-ts', # use the URL to the github repo
+  download_url = 'https://github.com/hawk31/nnet-ts/archive/master.zip',
+  keywords = ['time series', 'neural network', 'machine learning'],
+  classifiers = ["Topic :: Scientific/Engineering :: Artificial Intelligence"]
+)
